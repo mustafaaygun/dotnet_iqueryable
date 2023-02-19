1ab9b3251afb417a92cc6d2d0dca7ccecf36ff5d
mustafaaygun <mustafa-aygun-1@hotmail.com>
2023-02-19 12:33:29 +0300

    duzenleme

--- iqueryable/Program.cs
+++ iqueryable/Program.cs
@@ -5,42 +5,181 @@
         public int LNGKOD { get; set; }
         public string TXTAD { get; set; }
     }
+
+    public class TBLMUSTERIID
+    {
+        public int LNGKOD { get; set; }
+    }
     static void Main(string[] args)
     {
         var musteriler = new List<TBLMUSTERI>();
         musteriler.Add(new TBLMUSTERI { LNGKOD = 1, TXTAD = "Sabancı Holding" });
         musteriler.Add(new TBLMUSTERI { LNGKOD = 2, TXTAD = "Koç Holding" });
+
+/************************************************************************************************************************************************************/
+        /*
+            @Aggregate
+            Verilen değerleri toplar;
+        */
         var musteriBirlestir = musteriler.Aggregate("", (sonDeger, eleman) => sonDeger + " " + eleman.TXTAD);
+        /*
+            @return(
+                string => Sabancı Holding Koç Holding
+            )
+        */
         var carpim = musteriler.Aggregate(1, (sonDeger, eleman) => sonDeger * eleman.LNGKOD);
-        //Console.WriteLine($@"Carpim = {carpim}; Birlesim = {musteriBirlestir}");
-
-        var butunKayitlariKontrolEt = musteriler.All(x => x.TXTAD.EndsWith("g")); //butun kayitlar verilen kosula uyuyorsa true doner
-        //Console.WriteLine(butunKayitlariKontrolEt);
-        
-        var tekBirKayitUyuyorMu = musteriler.Any(x => x.TXTAD.StartsWith("S")); //herhangi bir kayit verilen kosulu saglarsa true doner
-        //Console.WriteLine(tekBirKayitUyuyorMu);
-
-        musteriler = musteriler.Append(new TBLMUSTERI { LNGKOD = 3, TXTAD = "Tekfen Holding" }).ToList(); //listenin sonuna kayıt ekler
-
-        var ieMusteriler = musteriler.AsEnumerable(); //ienumerable olarak dondurur
-
-        var average = musteriler.Average(x=>x.LNGKOD); //secilen degerin ortalamasini alir
-        //Console.WriteLine(average);
-        
-        var capacity = musteriler.Capacity; // kac adet eleman oldugunu verir : 3
-        //Console.WriteLine(capacity);
-       
-        musteriler.Cast<TBLMUSTERI>(); // convert eder
-
-        var ss = musteriler.Chunk(2);
-        
-var e = ss.FirstOrDefault()[0].TXTAD;
-        // musteriler = musteriler.Where(x => x.LNGKOD == 1).ToList(); //verilen kosul ile listeyi filtreler
-        foreach (var item in musteriler)
-        {
-            //Console.WriteLine($@"LNGKOD={item.LNGKOD} TXTAD={item.TXTAD}");
-        }
-
-        musteriler.AddRange(musteriler); // Listeyi aynı tip listeye ekler
+        /*
+            @return(
+                int => 2
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*  
+            @All
+            Butun kayitlar verilen kosula uyuyorsa true doner;
+        */
+        var butunKayitlariKontrolEt = musteriler.All(x => x.TXTAD.EndsWith("g"));
+        /*
+            @return(
+                boolean = True
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*
+            @Any
+            Herhangi bir kayit verilen kosulu saglarsa true doner;
+        */
+        var tekBirKayitUyuyorMu = musteriler.Any(x => x.TXTAD.StartsWith("S")); //
+        /*
+            @return(
+                boolean = True
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*
+            @Append
+            Listenin sonuna kayıt ekler;
+        */
+        musteriler = musteriler.Append(new TBLMUSTERI { LNGKOD = 3, TXTAD = "Tekfen Holding" }).ToList();
+        /*
+            @return(
+                musteriler = 
+                [0]:{
+                    LNGKOD [int]:1
+                    TXTAD [string]:"Sabancı Holding
+                }
+                [1]:{ 
+                    LNGKOD [int]:2
+                    TXTAD [string]:"Koç Holding"
+                }
+                [2]:{
+                    LNGKOD [int]:3
+                    TXTAD [string]:"Tekfen Holding"
+                }
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*
+            @AsEnumerable
+            ienumerable olarak dondurur
+        */
+        var ieMusteriler = musteriler.AsEnumerable(); //
+/************************************************************************************************************************************************************/
+        /*
+            @Average
+            Degerlerin ortalamasini alir
+        */
+        var average = musteriler.Average(x => x.LNGKOD);
+        /*
+            @return(
+                int => 2
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*
+            @Capacity
+            Kac adet eleman kapasitesi oldugunu verir;
+        */
+        var capacity = musteriler.Capacity;
+        /*
+           @return(
+               int => 3
+           )
+       */
+/************************************************************************************************************************************************************/
+        /*
+            @Cast
+            Listeyi icerisine verilen tipe donusturur;
+        */
+        var castMusteri = musteriler.Cast<TBLMUSTERI>().ToList(); // convert eder
+        /*
+            @return(
+                castMusteri = 
+                [0]:{
+                    LNGKOD [int]:1
+                    TXTAD [string]:"Sabancı Holding
+                }
+                [1]:{ 
+                    LNGKOD [int]:2
+                    TXTAD [string]:"Koç Holding"
+                }
+                [2]:{
+                    LNGKOD [int]:3
+                    TXTAD [string]:"Tekfen Holding"
+                }
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*
+            @Chunk
+            Listeyi verilen parcada boler;
+        */
+        var parcala = musteriler.Chunk(2);
+        /*
+            @return(
+                parcala = 
+                [0] [TBLMUSTERI[]]:
+                    [0]:{
+                        LNGKOD [int]:1
+                        TXTAD [string]:"Sabancı Holding
+                    }
+                    [1]:{ 
+                        LNGKOD [int]:2
+                        TXTAD [string]:"Koç Holding"
+                    }
+                [1] [TBLMUSTERI[]]:
+                    [0]:{
+                        LNGKOD [int]:3
+                        TXTAD [string]:"Tekfen Holding"
+                    }
+            )
+        */
+/************************************************************************************************************************************************************/
+        /*
+            @AddRange
+            Listeyi liste içerisine ekler;
+        */
+        musteriler.AddRange(new List<TBLMUSTERI>{new TBLMUSTERI{LNGKOD=4,TXTAD="Alarko Holding" }});
+        /*
+            @return(
+                castMusteri = 
+                [0]:{
+                    LNGKOD [int]:1
+                    TXTAD [string]:"Sabancı Holding
+                }
+                [1]:{ 
+                    LNGKOD [int]:2
+                    TXTAD [string]:"Koç Holding"
+                }
+                [2]:{
+                    LNGKOD [int]:3
+                    TXTAD [string]:"Tekfen Holding"
+                }
+                [3]:{
+                    LNGKOD [int]:4
+                    TXTAD [string]:"Alarko Holding"
+                }
+            )
+        */
     }
 }
